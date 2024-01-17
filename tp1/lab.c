ec1f48e67d95f4fefc5f9af7c3d6c5f78274294f
Omar Benzekri <omar.benzekri.2003@gmail.com>
2024-01-17 13:49:56 -0500

    partie 2

--- tp1/lab.c
+++ tp1/lab.c
@@ -1,17 +1,10 @@
-/* INF2610 - TP1
-/ Matricule 1 : 2244082
-/ Matricule 2 : 2209249
-*/
+#include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <stdio.h> 
 #include <stdbool.h>
 
 #define MAX_ID_LENGTH 256
 #define MAX_PLANE_TYPE_LENGTH 10
-#define NUM_WHEELS 4
+#define NUM_WHEELS 7
 #define NUM_WINGS 2
 #define NUM_PLANES 3
 
@@ -32,33 +25,61 @@ struct Plane {
     struct Wing wings[NUM_WINGS];
 };
 
+struct Wheel* createWheels(int id) {
+    struct Wheel* wheels = malloc(sizeof(struct Wheel) * NUM_WHEELS);
+
+    for (int i = 0; i < NUM_WHEELS; i++) {
+        wheels[i].id = id + i;
+        wheels[i].isRearWheel = (i >= 3);
+    }
+
+    return wheels;
+}
+
+void populateWingAttributes(struct Wing* wing, int id) {
+    for (int i = 0; i < 9; i++) {
+        wing->id[i] = (id % 10);
+        id /= 10;
+    }
+}
+
+struct Wing* createWings(long id) {
+    struct Wing* wings = malloc(sizeof(struct Wing) * NUM_WINGS);
+
+    for (int i = 0; i < NUM_WINGS; i++) {
+        populateWingAttributes(&wings[i], id + i);
+    }
+
+    return wings;
+}
+
+void createPlanes(struct Plane* planes, int id, int numberOfPlanes) {
+    for (int i = 0; i < numberOfPlanes; i++) {
+        snprintf(planes[i].id, MAX_ID_LENGTH, "%d", id + i);
+        planes[i].isAvailable = true;
+        createWheels(id);
+        createWings(id);
+    }
+}
 
 int main(int argc, char** argv) {
     printf("Hello\n");
 
-    int id = 1;
+    int id = 101;
 
     /* PARTIE 2 - [10 points] */
 
     /* Create wheel - [2 points] */
-    /*
-    Wheel[] wheels;
-    wheels = createWheels(id);    
-    */
+    struct Wheel* wheels = createWheels(id);
 
     /* Create wing - [4 points] */
-    /*
-    long longId = 1;
-    Wing[] wings;
-    wings = createWings(longId);
-    */
+    long longId = 123456;
+    struct Wing* wings = createWings(longId);
 
     /* Create plane - [4 points] */
-    /*
     int numberOfPlanes = NUM_PLANES;
-    Plane* planes = malloc(sizeof(Plane) * numberOfPlanes);
-    createPlanes(planes, *id, NUM_PLANES);
-    */
+    struct Plane* planes = malloc(sizeof(struct Plane) * numberOfPlanes);
+    createPlanes(planes, id, numberOfPlanes);
 
     /* PARTIE 3 - [6 points] */
 
