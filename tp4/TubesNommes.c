09dd2da9e180e536d72753fd2365a6d2d2c2fb1a
Omar Benzekri <omar.benzekri.2003@gmail.com>
2024-03-13 04:20:21 -0400

    OB | Question 2 NOT WORKING

--- tp4/TubesNommes.c
+++ tp4/TubesNommes.c
@@ -1,5 +1,40 @@
-// Ajouter les directives d'inclusion nécessaires
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
 
 int main() {
-    
+    mkfifo("pipe1", 0666);
+    mkfifo("pipe2", 0666);
+
+    if (fork() == 0) { // P3
+        int pipe1 = open("pipe1", O_RDONLY);
+        dup2(pipe1, STDIN_FILENO);
+        execlp("diff", "diff", "-", "In.txt", NULL);
+    }
+
+    int pipe1 = open("pipe1", O_WRONLY);
+
+    if (fork() == 0) { // P2
+        int pipe2 = open("pipe2", O_RDONLY);
+        dup2(pipe2, STDIN_FILENO);
+        dup2(pipe1, STDOUT_FILENO);
+        execlp("tr", "tr", "[a-z]", "[A-Z]", NULL);
+    }
+
+    int pipe2 = open("pipe2", O_WRONLY);
+
+    if (fork() == 0) { // P1
+        dup2(pipe2, STDOUT_FILENO);
+        execlp("cat", "cat", "In.txt", NULL);
+    }
+
+    wait(NULL);
+    wait(NULL);
+    wait(NULL);
+
+    unlink("pipe1");
+    unlink("pipe2");
+
+    return 0;
 }
