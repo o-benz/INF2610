2df4a0f521f39a2409655805cf14d552cbb22aab
MaelPixel <113027326+MaelPixel@users.noreply.github.com>
2024-03-16 13:24:23 -0400

    TubesNommes fonctionne, proposition pour anonymes

--- tp4/TubesAnonymes.c
+++ tp4/TubesAnonymes.c
@@ -8,40 +8,51 @@ int main() {
     pipe(pipe1);
     pipe(pipe2);
 
+    // P3.
     if (fork() == 0) {
-        dup2(pipe1[1], STDOUT_FILENO);
-        close(pipe1[0]);
+        close(pipe1[0]); 
         close(pipe1[1]);
-        close(pipe2[0]);
-        close(pipe2[1]);
-        execlp("cat", "cat", "In.txt", NULL);
-    }
 
-    if (fork() == 0) {
-        dup2(pipe1[0], STDIN_FILENO);
-        dup2(pipe2[1], STDOUT_FILENO);
-        close(pipe1[0]);
-        close(pipe1[1]);
-        close(pipe2[0]);
-        close(pipe2[1]);
-        execlp("tr", "tr", "[a-z]", "[A-Z]", NULL);
-    }
-
-    close(pipe1[0]);
-    close(pipe1[1]);
-
-    if (fork() == 0) {
         dup2(pipe2[0], STDIN_FILENO);
         int fd = open("Out.txt", O_WRONLY | O_CREAT | O_TRUNC, 0660);
         dup2(fd, STDOUT_FILENO);
+        close(fd);
         close(pipe2[0]);
         close(pipe2[1]);
+
+        // P2.
+        if (fork() == 0) {
+            pipe(pipe1); 
+            if (fork() == 0) {
+                dup2(pipe1[1], STDOUT_FILENO);
+                close(pipe1[0]);
+                close(pipe1[1]);
+                execlp("cat", "cat", "In.txt", NULL);
+                _exit(1); 
+            }
+
+            dup2(pipe1[0], STDIN_FILENO);
+            dup2(pipe2[1], STDOUT_FILENO);
+            close(pipe1[0]);
+            close(pipe1[1]);
+            close(pipe2[0]);
+            close(pipe2[1]);
+            execlp("tr", "tr", "[a-z]", "[A-Z]", NULL);
+            _exit(1); 
+        }
+
+        close(pipe2[1]); 
         execlp("diff", "diff", "-", "In.txt", NULL);
+        _exit(1); 
     }
 
+    
+    close(pipe1[0]);
+    close(pipe1[1]);
     close(pipe2[0]);
     close(pipe2[1]);
 
+    
     while (wait(NULL) > 0);
 
     return 0;
--- tp4/TubesNommes.c
+++ tp4/TubesNommes.c
@@ -1,4 +1,6 @@
 #include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
@@ -7,27 +9,43 @@ int main() {
     mkfifo("pipe1", 0666);
     mkfifo("pipe2", 0666);
 
-    if (fork() == 0) { // P3
+    // P3: diff
+    if (fork() == 0) {
         int pipe1 = open("pipe1", O_RDONLY);
         dup2(pipe1, STDIN_FILENO);
+        close(pipe1); 
         execlp("diff", "diff", "-", "In.txt", NULL);
+        exit(0); 
     }
 
     int pipe1 = open("pipe1", O_WRONLY);
 
-    if (fork() == 0) { // P2
+    // P2: tr
+    if (fork() == 0) {
         int pipe2 = open("pipe2", O_RDONLY);
         dup2(pipe2, STDIN_FILENO);
+        close(pipe2); 
+
         dup2(pipe1, STDOUT_FILENO);
+        close(pipe1); 
+
         execlp("tr", "tr", "[a-z]", "[A-Z]", NULL);
+        exit(0); 
     }
+
+    close(pipe1);
 
     int pipe2 = open("pipe2", O_WRONLY);
 
-    if (fork() == 0) { // P1
+    // P1: cat
+    if (fork() == 0) {
         dup2(pipe2, STDOUT_FILENO);
+        close(pipe2); 
         execlp("cat", "cat", "In.txt", NULL);
+        exit(0); 
     }
+
+    close(pipe2);
 
     wait(NULL);
     wait(NULL);
